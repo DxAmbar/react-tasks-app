f1f13f084d856083d05ff70ffcd046c36537ac74
DxAmbar <d.ambarbaeza@gmail.com>
2023-02-07 01:23:29 -0300

    Intalación de Taildwind y estilizado

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -19,20 +19,23 @@ function TaskForm({  }) {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-900 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
         <input
           placeholder="Escribe tu tarea"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
+          className="bg-slate-300 p-3 w-full mb-4"
           autoFocus
         />
         <textarea
           placeholder="Descripción de la tarea"
           onChange={(e) => setDescription(e.target.value)}
           value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
         ></textarea>
-        <button>Guardar</button>
+        <button className="bg-indigo-500 px-3 py-1 text-white ">Guardar</button>
       </form>
     </div>
   );
